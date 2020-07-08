b396757c2c7ef3e8a8dab22717cb2a08d24b9ffc
Darktechie-programmer <kumaramit26037@gmail.com>
2020-07-08 16:07:34 +0530

    comments added

---
+++ app.py
@@ -0,0 +1,151 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+#Load the Random Forest Classifier model
+filename = 'score-lr-model.pkl'
+regressor = pickle.load(open(filename,'rb'))
+
+
+app = Flask(__name__)
+
+Score = []
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+        
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        
+        data = np.array([temp_array])
+        my_prediction = int(regressor.predict(data)[0])
+        Score.append(my_prediction)
+              
+        return render_template('result.html', lower_limit = my_prediction-5, upper_limit = my_prediction+5)
+
+@app.route('/team2')
+def team2():
+    return render_template('team2.html')
+
+@app.route('/predict2', methods=['POST'])
+def predict2():
+    temp_array = list()
+    
+    if request.method == 'POST':
+        
+        batting_team = request.form['batting-team']
+        if batting_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif batting_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif batting_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif batting_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif batting_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif batting_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif batting_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif batting_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        bowling_team = request.form['bowling-team']
+        if bowling_team == 'Chennai Super Kings':
+            temp_array = temp_array + [1,0,0,0,0,0,0,0]
+        elif bowling_team == 'Delhi Daredevils':
+            temp_array = temp_array + [0,1,0,0,0,0,0,0]
+        elif bowling_team == 'Kings XI Punjab':
+            temp_array = temp_array + [0,0,1,0,0,0,0,0]
+        elif bowling_team == 'Kolkata Knight Riders':
+            temp_array = temp_array + [0,0,0,1,0,0,0,0]
+        elif bowling_team == 'Mumbai Indians':
+            temp_array = temp_array + [0,0,0,0,1,0,0,0]
+        elif bowling_team == 'Rajasthan Royals':
+            temp_array = temp_array + [0,0,0,0,0,1,0,0]
+        elif bowling_team == 'Royal Challengers Bangalore':
+            temp_array = temp_array + [0,0,0,0,0,0,1,0]
+        elif bowling_team == 'Sunrisers Hyderabad':
+            temp_array = temp_array + [0,0,0,0,0,0,0,1]
+            
+            
+        overs = float(request.form['overs'])
+        runs = int(request.form['runs'])
+        wickets = int(request.form['wickets'])
+        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
+        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
+        
+        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
+        
+        data = np.array([temp_array])
+        my_prediction = int(regressor.predict(data)[0])
+        Score.append(my_prediction)
+        
+        return render_template('team2result.html', lower_limit = my_prediction-5, upper_limit = my_prediction+5)
+
+@app.route('/winner')
+def winner():
+    if Score[0] > Score[1]:
+        team = 'Team1'
+    else:
+        team = 'Team2'
+    return render_template('winner.html',team=team)
+
+
+# if __name__ == 'main':  ## not worked here
+app.run(debug=True,host="localhost")  #host="localhost",port=5000
+    
+
